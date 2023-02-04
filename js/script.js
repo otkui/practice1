9f82b4b571dcf0a2144ac2138d82c2df009ee5cc
OTK <124166368+otkui@users.noreply.github.com>
2023-02-04 10:19:20 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(".slider").slick({
+  // 自動再生
+  autoplay: true,
+  // ３秒で次の画像へ
+  autoplaySpeed: 3000,
+  // ページャーをつける
+  dots: true,
+  // フェードさせる
+  fade: true,
+  // ２秒かけて切り替わる
+  speed: 2000,
+});
