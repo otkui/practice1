b3f71e912dc7212d3dfac299aaab11592b8f0a6e
OTK <124166368+otkui@users.noreply.github.com>
2023-02-10 21:24:37 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+// モバイルメニュー
+$(".hmenu").click(function () {
+  // Aパターン
+  // $("nav").slideToggle(500);
+  $(this).toggleClass("close");
+  return false;
+});
+
+// Bパターン
+$(".container").click(function () {
+  $(".hmenu").removeClass("close");
+});
+// ～～～～～～～～～～～～～～～～
+
 $(".slider").slick({
   // 自動再生
   autoplay: true,
@@ -9,4 +23,18 @@ $(".slider").slick({
   fade: true,
   // ２秒かけて切り替わる
   speed: 2000,
+});
+
+// ページトップ表示のパターン
+// ある程度スクロールしたらフェード表示する
+$(window).scroll(function () {
+  var sc = $(window).scrollTop();
+  // 検証→コンソール表示でどこをスクロールしてる（数値）かわかる
+  // console.log(sc);
+  // scが7００を超えたら表示
+  if (sc > 700) {
+    $("footer p.pagetop").fadeIn(500);
+  } else {
+    $("footer p.pagetop").fadeOut(500);
+  }
 });
